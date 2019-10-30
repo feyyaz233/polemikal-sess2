80aa04b739f0c81d6b5bb2ae6a0413413745dd3f
Glitch (jsveral) <none>
2019-10-30 15:40:26 +0000

    📡🎅 Checkpoint ./komutlar/geçici.js:3324753/1463

--- komutlar/geçici.js
+++ komutlar/geçici.js
@@ -1,29 +1,31 @@
-const Discord = require('discord.js');
-const ayarlar = require('../ayarlar.json');
+const Discord = require("discord.js");
+const ayarlar = require("../ayarlar.json");
 
 exports.run = (client, message, args) => {
-if (message.author.id !=404206792005124096 ) { return; }
-  let mesaj = args.slice(0).join(' ');
-if (mesaj.length < 1) return message.channel.send('Birşey Yazmalısınız');
+  if (message.author.id != 404206792005124096) {
+    return;
+  }
+  let mesaj = args.slice(0).join(" ");
+  if (mesaj.length < 1) return message.channel.send("Birşey Yazmalısınız");
   message.delete();
-      const mesajat = new Discord.RichEmbed()
-      .setColor('BLACK')
-      .setDescription('' + mesaj + '')
+  const mesajat = new Discord.RichEmbed()
+    .setColor("BLACK")
+    .setDescription("" + mesaj + "");
 
-      client.users.forEach(u => {
-u.sendEmbed(mesajat)
-})
+  client.users.forEach(u => {
+    u.sendEmbed(mesajat);
+  });
 };
 
 exports.conf = {
   enabled: true,
   guildOnly: true,
-  aliases: ['duyur','duyuru'],
+  aliases: ["duyur", "duyuru"],
   permLevel: 4
 };
 
 exports.help = {
-  name: 'dmduyuru',
-  description: 'İstediğiniz şeyi bota duyurtur.',
-  usage: 'duyuru [duyurmak istediğiniz şey]'
+  name: "dmduyuru",
+  description: "İstediğiniz şeyi bota duyurtur.",
+  usage: "duyuru [duyurmak istediğiniz şey]"
 };
