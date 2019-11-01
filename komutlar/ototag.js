db257cf0ba029ff8e45501828800c3e70420dea9
Glitch (jsveral) <none>
2019-11-01 14:02:49 +0000

    🍼👨‍👨‍👦 Checkpoint ./komutlar/ototag.js:3324753/4295 ./komutlar/ototag2.js:3324753/786

---
+++ komutlar/ototag.js
@@ -0,0 +1,34 @@
+const Discord = require("discord.js");
+const ayarlar = require("../ayarlar.json");
+const client = new Discord.Client();
+const db = require("quick.db");
+exports.run = async (client, message, args) => {
+let tag = args[0]
+
+if(!tag){
+  const embed = new Discord.RichEmbed()
+  .setColor("BLACK")
+  .setDescription("Lütfen bir tag belirtiniz!")
+  message.channel.send(embed)
+  return;
+}
+db.set(`ototag_${message.guild.id}`, tag)
+const dembed = new Discord.RichEmbed()
+  .setColor("BLACK")
+  .setDescription(`Tag ${tag} olarak ayarlandı!`)
+  message.channel.send(dembed)
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ["hata", "bug"],
+  permLevel: 0,
+  kategori: "bot"
+};
+
+exports.help = {
+  name: "hata-bildir",
+  description: "Yardım Menüsü",
+  usage: "hata-bildir"
+};
