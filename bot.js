a42b7cd42ec103cf5c514d5b6c6564ab0fc1c946
Glitch (jsveral) <none>
2019-11-02 13:30:40 +0000

    🐧💴 Checkpoint ./bot.js:3324753/1822 ./komutlar/test.js:3324753/1099

--- bot.js
+++ bot.js
@@ -181,6 +181,46 @@ function kanaladı3() {
       });
 }
 
+//
+
+
+client.on('guildMemberAdd',async member => {
+  let user = client.users.get(member.id);
+  let chan = client.channels.get(db.fetch(`guvenlik${member.guild.id}`)) 
+       const Canvas = require('canvas')
+       const canvas = Canvas.createCanvas(360,100);
+       const ctx = canvas.getContext('2d');
+  
+  const resim1 = await Canvas.loadImage('https://cdn.discordapp.com/attachments/591299755976425493/614151181752860672/yhosgeldirrn.png')
+    const resim2 = await Canvas.loadImage('https://cdn.discordapp.com/attachments/591299755976425493/614164419768877056/yhosgeldirrn.png')
+    const kurulus = new Date().getTime() - user.createdAt.getTime();
+    const gün = moment(kurulus).format('dddd');  
+    var kontrol;
+      if (kurulus > 2629800000) kontrol = resim2
+    if (kurulus < 2629800000) kontrol = resim1
+
+       const background = await Canvas.loadImage('https://cdn.discordapp.com/attachments/591299755976425493/614164413318168606/Adsz.png');
+       ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+   
+
+
+  const avatar = await Canvas.loadImage(member.user.displayAvatarURL);
+  ctx.drawImage(kontrol,0,0,canvas.width, canvas.height)
+  ctx.beginPath();
+    ctx.lineWidth = 4;
+  ctx.fill()
+    ctx.lineWidth = 4;
+  ctx.arc(180, 46, 36, 0, 2 * Math.PI);
+    ctx.clip();
+  ctx.drawImage(avatar, 143,10, 73, 72  );
+
+   
+       const attachment = new Discord.Attachment(canvas.toBuffer(), 'atomgüvenlik.png');
+    chan.send(attachment)
+});
+
+
+///
 //nasıl iş bu ameka
 //silindiği için adını alamıyo o yüzden açamıyor? o zaman loga selam verirdi
 //true 7
@@ -235,10 +275,7 @@ client.on("guildMemberAdd", async (guild, member, user) => {
   if (!tag) {
     return;
   }
-
-  isim.replace(`-kullanıcı-`, `${member.user.username}`);
-  isim.replace(`-tag-`, `${tag}`);
-  member.setNickname(`${isim}`);
+  member.setNickname(isim ? isim.replace('-tag-', `\`${tag}\``).replace('-kullanıcı-',`\`${member.user.tag}\``) : ``)
 });
 client.on("guildMemberAdd", member => {
   try {
