79ecc78f7e635cc0503177e986c2a3d28b954c43
Glitch (jsveral) <none>
2019-12-28 12:44:36 +0000

    ⚓️🌉 Checkpoint ./komutlar/js.js:3324753/2063

--- komutlar/js.js
+++ komutlar/js.js
@@ -2,26 +2,11 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 
 exports.run = (client, message, params) => {
-  const DBL = require("dblapi.js");
-  const dbl = new DBL(
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0MjQzNjIyMzMxNDU1ODk3NiIsImJvdCI6dHJ1ZSwiaWF0IjoxNTc2MzIwMDMxfQ.fbbZYB4SuE42yGFuvSASSbiYt6XTYi_VcmmyghLo7Fw",
-    client
-  );
-
-  dbl.hasVoted(message.author.id).then(voted => {
-    if (voted) {
-      var role = message.guild.roles.find(role => role.id === "655846401867120680"); //role.id bölümünü role.name yazarak id yerine ad girebilirsiniz
-      if (message.member.roles.has(role.id))
-        return message.channel.send("Zaten rolü almışsın!"); //role.name yaparsanız burada ki role.id yazısınıda düzeltin
-      message.member.addRole(role);
-      message.channel.send(`JavaScript rolü başarıyla verildi!`); //komutu kullanınca size atacağı mesaj
-      
-    } else {
-      message.channel.send(
-        "Bu komutu kullanabilmek için 12 saatte bir oy vermelisiniz!\nSite: https://top.gg/bot/642436223314558976/vote\nNot: Eğer oy verdiyseniz sisteme düşmesini bekleyiniz."
-      );
-    }
-  });
+  var role = message.guild.roles.find(role => role.id === "655846401867120680"); //role.id bölümünü role.name yazarak id yerine ad girebilirsiniz
+  if (message.member.roles.has(role.id))
+    return message.channel.send("Zaten rolü almışsın!"); //role.name yaparsanız burada ki role.id yazısınıda düzeltin
+  message.member.addRole(role);
+  message.channel.send(`JavaScript rolü başarıyla verildi!`); //komutu kullanınca size atacağı mesaj
 };
 
 exports.conf = {
