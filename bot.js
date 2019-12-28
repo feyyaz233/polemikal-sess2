bd576a2daa6a8d927f232538854b32915ba995ec
Glitch (jsveral) <none>
2019-12-28 19:41:27 +0000

    🐡😎 Checkpoint ./bot.js:3324753/9337

--- bot.js
+++ bot.js
@@ -127,40 +127,38 @@ client.on("message", msg => {
 //
 
 function s1() {
-    return new Promise(resolve => {
-        setTimeout(() => {
-            client.guilds.get(`Swİd`).setName(`İsim 1`);
-            s2();
-        }, 3000);
-      });
+  return new Promise(resolve => {
+    setTimeout(() => {
+      client.guilds.get(`Swİd`).setName(`İsim 1`);
+      s2();
+    }, 3000);
+  });
 }
 function s2() {
-    return new Promise(resolve => {
-        setTimeout(() => {
-            client.guilds.get(`SwId`).setName(`İsim 2`);
-            s3ĺ
-            
-            ĺ();
-        }, 3000);
-      });
+  return new Promise(resolve => {
+    setTimeout(() => {
+      client.guilds.get(`SwId`).setName(`İsim 2`);
+
+      s3();
+    }, 3000);
+  });
 }
-function kanaladı3() {
-    return new Promise(resolve => {
-        setTimeout(() => {
-            client.channels.get(`kanal id`).setName(`Kanal ismi 2`);
-            kanaladı4();
-        }, 10000);
-      });
+function s3() {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      client.channels.get(`kanal id`).setName(`İsim 2`);
+      s4();
+    }, 300);
+  });
 }
 function kanaladı4() {
-    return new Promise(resolve => {
-        setTimeout(() => {
-            client.channels.get(`kanal id`).setName(`Kanal ismi 2`);
-            s1();
-        }, 10000); // kanal adının değişme hızı düşürmeyin kasma yaratır !
-      });
+  return new Promise(resolve => {
+    setTimeout(() => {
+      client.channels.get(`kanal id`).setName(`Kanal ismi 2`);
+      s1();
+    }, 10000); // kanal adının değişme hızı düşürmeyin kasma yaratır !
+  });
 }
-
 
 client.elevation = message => {
   if (!message.guild) {
