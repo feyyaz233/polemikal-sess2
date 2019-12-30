8a41c430e0ed2d5cede718ab6e7a4940e14e2e1d
Glitch (jsveral) <none>
2019-12-30 17:07:25 +0000

    🍣🌧 Checkpoint ./komutlar/test.js:3324753/894

--- komutlar/test.js
+++ komutlar/test.js
@@ -2,36 +2,20 @@ const Discord = require("discord.js"),
       db = require("quick.db")
 
 exports.run = async (client, message, args) => {
-  let enis = args[0];
-  let veri = await db.fetch(`hg-bb_${message.guild.id}`);
-  if (!enis)
-    return message.channel.send("Açılsın mı kapansın mı? !hg-bb aç && kapat");
-  if (enis == "aç") {
-    if (veri) return message.channel.send("Zaten hg bb açık1");
-    else {
-      db.set(`hg-bb_${message.guild.id}`, `hhhh`);
-      return;
-    }
-    return;
-  }
-  if (enis == "kapat") {
-    if (!veri) return message.channel.send("Zaten hg bb kapalı!");
-    else {
-      db.delete(`hg-bb_${message.guild.id}`);
-      return;
-    }
-    return;
-  }
+let kisi = message.mentions.users.first()
+if(!kisi) return message.channel.send("Lütfen birini etiketle")
+  db.delete(`lastDaily_${kisi.id}`)
+  message.channel.send("Başarılı!")
 };
 exports.conf = {
   enabled: true,
   guildOnly: true,
   aliases: [],
-  permLevel: 2,
+  permLevel: 4,
   kategori: "yapımcı"
 };
 exports.help = {
-  name: "hg-bb",
-  description: "hg-bb",
-  usage: "hg-bb"
+  name: "günlük-sıfırla",
+  description: "günlük-sıfırla",
+  usage: "günlük-sıfırla"
 };
