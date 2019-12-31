f45b6389afcac8612248efda7372b81fc7397479
Glitch (jsveral) <none>
2019-12-31 18:19:28 +0000

    🎖⛄️ Checkpoint ./bot.js:3324753/82 ./komutlar/kanalkoruma.js:3324753/978 ./komutlar/kanalkoruma2.js:3324753/1121

--- bot.js
+++ bot.js
@@ -272,14 +272,16 @@ client.on("guildMemberAdd", async member => {
 });
 
 client.on("channelDelete", async channel => {
-  let kanal = await db.fetch(`rolk_${channel.guild.id}`);
+  let kanal = await db.fetch(`kkk_${channel.guild.id}`);
   if (!kanal) return;
   const entry = await channel.guild
     .fetchAuditLogs({ type: "CHANNEL_DELETE" })
     .then(audit => audit.entries.first());
   if (entry.executor.id == client.user.id) return;
-  //if (entry.executor.hasPermission("ADMINISTRATOR")) return;
-  channel.guild.createChannel(channel.name);
+  if (entry.executor.hasPermission("ADMINISTRATOR")) return;
+  channel.guild.createChannel(channel.name, 'text', [{
+    id: channel.guild.id
+  }]);
 
   const embed = new Discord.RichEmbed()
     .setTitle(`Bir kanal silindi!`)
@@ -290,7 +292,7 @@ client.on("channelDelete", async channel => {
 });
 
 client.on("channelCreate", async channel => {
-  let kanal = await db.fetch(`rolk_${channel.guild.id}`);
+  let kanal = await db.fetch(`kkk_${channel.guild.id}`);
   if (!kanal) return;
   const entry = await channel.guild
     .fetchAuditLogs({ type: "CHANNEL_CREATE" })
