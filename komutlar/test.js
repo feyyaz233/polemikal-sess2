f90319ecd6a71fe606ffce6518bf27ed636515e1
Glitch (jsveral) <none>
2019-12-31 13:16:24 +0000

    🕵🤗 Checkpoint ./komutlar/test.js:3324753/1893 ./package.json:3324753/3660

--- komutlar/test.js
+++ komutlar/test.js
@@ -1,11 +1,12 @@
 const Discord = require("discord.js"),
-      db = require("quick.db")
-
+  bs = require("brawlstars");
+const client2 = new bs.Client({
+  token:
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkaXNjb3JkX3VzZXJfaWQiOiI0MDQyMDY3OTIwMDUxMjQwOTYiLCJyZWFzb24iOiJEYXZldCBZw7ZuZXRpY2lzaSIsInZlcnNpb24iOjEsImlhdCI6MTU3Nzc5ODEzMH0.24x3Js8gHPGLp4dYaWNyxRWeOgrzXqGNUMRUJrrw7Oo"
+});
 exports.run = async (client, message, args) => {
-let kisi = message.mentions.users.first()
-if(!kisi) return message.channel.send("Lütfen birini etiketle")
-  db.delete(`lastDaily_${kisi.id}`)
-  message.channel.send("Başarılı!")
+  const player = await client.getPlayer("#C2L0");
+  console.log(player);
 };
 exports.conf = {
   enabled: true,
@@ -15,7 +16,7 @@ exports.conf = {
   kategori: "yapımcı"
 };
 exports.help = {
-  name: "günlük-sıfırla",
-  description: "günlük-sıfırla",
-  usage: "günlük-sıfırla"
+  name: "bs",
+  description: "bs",
+  usage: "bs"
 };
