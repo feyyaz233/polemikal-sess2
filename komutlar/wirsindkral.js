bcbd90bb1d575057289eb574a089dc5fa75c492a
Glitch (jsveral) <none>
2020-01-01 13:35:42 +0000

    💕🕍 Checkpoint ./komutlar/wirsindkral.js:3324753/161 ./ayarlar.json:3324753/4 ./komutlar/wirsindkral3.js:3324753/3423

--- komutlar/wirsindkral.js
+++ komutlar/wirsindkral.js
@@ -5,7 +5,7 @@ exports.run = async (client, message, args) => {
   let prefix = (await db.fetch(`prefix_${message.guild.id}`)) || "!";
   let veri = await db.fetch(`rol1_${message.guild.id}`);
   let veri2 = await db.fetch(`rol2_${message.guild.id}`);
-  let veri12 = await db.fetch(`roldavet1_${messgae.guild.id}`)
+  let veri12 = await db.fetch(`roldavet1_${message.guild.id}`)
   if (veri2) {
     const embed = new Discord.RichEmbed()
       .setDescription(`Zaten maksimum rütbe sınırına ulaşmışsınız!`)
@@ -87,10 +87,12 @@ exports.run = async (client, message, args) => {
 
     message.channel.send(embed);
 if(enis < veri12){
+  let hhh = await db.fetch(`rol1_${message.guild.id}`)
+  let sss = await db.fetch(`roldavet1_${message.guild.id}`)
   await db.set(`rol1_${message.guild.id}`, sine.id);
     await db.set(`roldavet1_${message.guild.id}`, enis);
-await db.set(`rol2_${message.guild.id}`, sine.id);
-    await db.set(`roldavet2_${message.guild.id}`, veri12);
+await db.set(`rol2_${message.guild.id}`, hhh);
+    await db.set(`roldavet2_${message.guild.id}`, sss);
   
     return
 }else{
