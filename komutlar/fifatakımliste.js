94b46a763aad57e8c954f46325ec858703de446e
Glitch (jsveral) <none>
2020-01-03 14:34:01 +0000

    🎬🍎 Checkpoint ./komutlar/fifatakımliste.js:3324753/1429 ./bot.js:3324753/54 ./package.json:3324753/3733

--- komutlar/fifatakımliste.js
+++ komutlar/fifatakımliste.js
@@ -1,14 +1,26 @@
 const fnt = require("fortnitetracker-7days-stats");
+const Discord = require("discord.js")
 exports.run = async (client, message, args) => {
   let platform = args[0]
   let user = args.slice(1).join(" ")
   
-  if(!platform) return message.channel.send("Lütfen bir platform belirtiniz!\nMevcut platformlar: \`pc\`, \psn, xbl")
+  if(!platform) return message.channel.send("Lütfen bir platform belirtiniz!\nMevcut platformlar: \`pc\`, \`psn\`, \`xbl\`")
+  if(!user) return message.channel.send("Lütfen bir kullanıcı belirtiniz!")
   fnt.getStats(user, platform, (err, result) => {
     if (err) {
-      console.log(err.message); // player not found
+      message.channel.send("Hata var!")
     } else {
-      console.log("Wins in the last 7 days: " + result.wins);
+      const embed = new Discord.RichEmbed()
+      .setAuthor(result.accountName)
+      .addField("Platform", result.platform, true)
+      .addField("Skor", result.score, true)
+      .addField("Toplam Öldürme", result.kills, true)
+      .addField("Toplam Zafer", result.wins, true)
+      .addField("Oynalınan Maç", result.matches, true)
+      console.log(result.top_3_5_10)
+      console.log(result.top_6_12_25)
+      .setThumbnail(result.skinUrl)
+      
     }
   });
 };
