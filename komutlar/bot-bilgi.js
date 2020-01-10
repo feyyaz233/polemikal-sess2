32cd8904d59e05032205a3fde468f9ef98a686fb
Glitch (jsveral) <none>
2020-01-10 17:22:21 +0000

    💡☺️ Checkpoint ./komutlar/bot-bilgi.js:3324753/19122 ./komutlar/yardım.js:3324753/58 ./komutlar/davet.js:3324753/4282

--- komutlar/bot-bilgi.js
+++ komutlar/bot-bilgi.js
@@ -3,40 +3,60 @@ const db = require("quick.db");
 const moment = require("moment");
 require("moment-duration-format");
 module.exports.run = async (bot, message) => {
-  let premium2 = await db.fetch(`pre_${message.guild.id}`)
-  let pre
-  if(!premium2){
-    pre = "Hayır!"
-  }else{
-    pre = "Evet!"
+  let kontrol = await db.fetch(`dil_${message.guild.id}`);
+  if (kontrol == "EN_us") {
+    const duration = moment
+      .duration(bot.uptime)
+      .format("D [days], H [hours], m [minutes], s [seconds]");
+    const embed = new Discord.RichEmbed()
+      .setColor("BLACK")
+      .addField(`Version`, `0.1`, true)
+      .addField(`Activity Time`, duration, true)
+      .addField(`Guilds`, bot.guilds.size.toLocaleString(), true)
+      .addField(`Members`, bot.users.size.toLocaleString(), true)
+      .addField(`Ping`, bot.ping + "ms", true)
+      .addField(
+        `Used Ram`,
+        `%${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}`,
+        true
+      )
+      .addField(`Support Server`, `[Click!](https://discord.gg/7cbNRQF)`, true)
+      .addField(`Add Bot`, `[Click!](https://discordapp.com/oauth2/authorize?client_id=665232633529368576&scope=bot&permissions=8)`, true)
+      .addField(`Vote The Bot`, `Coming Soon!`, true)
+      .setFooter(bot.user.username, bot.user.avatarURL);
+    message.channel.send(embed);
+  } else {
+    const duration = moment
+      .duration(bot.uptime)
+      .format("D [gün], H [saat], m [dakika], s [saniye]");
+    const embed = new Discord.RichEmbed()
+      .setColor("BLACK")
+      .addField(`Versiyon`, `0.1`, true)
+      .addField(`Aktiflik Süresi`, duration, true)
+      .addField(`Sunucular`, bot.guilds.size.toLocaleString(), true)
+      .addField(
+        `Kullanıcılar`,
+        bot.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString(),
+        true
+      )
+      .addField(`Gecikme`, bot.ping + "ms", true)
+      .addField(
+        `Ram Kullanımı`,
+        `%${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}`,
+        true
+      )
+      .addField(`Destek Sunucusu`, `[Tıkla!](https://discord.gg/7cbNRQF)`, true)
+      .addField(`Botu Ekleyin`, `[Tıkla!](https://discordapp.com/oauth2/authorize?client_id=665232633529368576&scope=bot&permissions=8)`, true)
+      .addField(`Bota Oy Verin`, `Yakında`, true)
+      .setFooter(bot.user.username, bot.user.avatarURL);
+    message.channel.send(embed);
   }
-  const duration = moment
-    .duration(bot.uptime)
-    .format("D [gün], H [saat], m [dakika], s [saniye]");
-  const embed = new Discord.RichEmbed()
-    .setColor("BLACK")
-    .addField(`Versiyon`, `0.1`, true)
-    .addField(`Aktiflik Süresi`, duration, true)
-    .addField(`Sunucular`, bot.guilds.size.toLocaleString(), true)
-    .addField(`Kullanıcılar`, bot.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString(), true)
-    .addField(`Ping`, bot.ping + "ms", true)
-    .addField(
-      `Ram Kullanımı`,
-      `%${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}`,
-      true
-    )
-    .addField(`Bu Sunucu Premium Mu?`, pre, false)
-    .addField(`Destek Sunucusu`, `xxx`, false)
-    .addField(`Botu Ekleyin`, `xxx`, false)
-  .addField(`Bota Oy Verin`, `xxx`, false)
-    .setFooter(bot.user.username, bot.user.avatarURL);
-  message.channel.send(embed);
 };
 
 module.exports.conf = {
   enabled: true,
   guildOnly: false,
-  aliases: [],
+  aliases: ["bot-info"],
   permLevel: 0,
   kategori: "sunucu"
 };
--- komutlar/davet.js
+++ komutlar/davet.js
@@ -5,13 +5,9 @@ module.exports.run = async (client, message, args) => {
   let kontrol = await db.fetch(`dil_${message.guild.id}`);
   let prefix = (await db.fetch(`prefix_${message.guild.id}`)) || "!";
   if (kontrol == "EN_us") {
-   const embed = new Discord.RichEmbed()
+    const embed = new Discord.RichEmbed()
       .setDescription(
-        `Bot Version; **v0.1**, Prefix: **${prefix}**, Language: **${kontrol}**`
-      )
-      .addField(
-        `Bot`,
-        `\`help\`, \`language\`, \`bot-info\``
+        `[Click Me!](https://discordapp.com/oauth2/authorize?client_id=665232633529368576&scope=bot&permissions=8)`
       )
 
       .setColor("BLACK")
@@ -19,15 +15,11 @@ module.exports.run = async (client, message, args) => {
     message.channel.send(embed);
     return;
   } else {
-  
     const embed = new Discord.RichEmbed()
       .setDescription(
-        `Bot sürümü; **v0.1**, Prefix: **${prefix}**, Dil: **${kontrol}**`
+        `[Tıkla!](https://discordapp.com/oauth2/authorize?client_id=665232633529368576&scope=bot&permissions=8)`
       )
-      .addField(
-        `Bot`,
-        `\`yardım\`, \`dil\`, \`bot-bilgi\``
-      )
+      
 
       .setColor("BLACK")
       .setFooter(client.user.username, client.user.avatarURL);
@@ -38,12 +30,12 @@ module.exports.run = async (client, message, args) => {
 exports.conf = {
   enabled: true,
   guildOnly: false,
-  aliases: ["y", "help", "h"],
+  aliases: ["invite"],
   permLevel: 0
 };
 
 exports.help = {
-  name: "yardım",
-  description: "yardım",
-  usage: "yardım"
+  name: "davet",
+  description: "davet",
+  usage: "davet"
 };
