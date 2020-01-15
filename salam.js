f04fd5354e9e21a2431ab269e5ada6209932e2ec
Glitch (jsveral) <none>
2020-01-15 07:02:56 +0000

    🏮💕 Checkpoint ./bot.js:3324753/22 ./salam.js:3324753/46761

--- salam.js
+++ salam.js
@@ -4,20 +4,22 @@ const Discord = require("discord.js"),
 module.exports.run = async (client, message, args) => {
   let prefix = (await db.fetch(`prefix_${message.guild.id}`)) || "!";
 
-  let kanal = await db.fetch(`kanalk_${message.guild.id}`);
+  let kanal = args[0];
   if (!kanal) {
     const embed = new Discord.RichEmbed()
       .setColor("BLACK")
       .setFooter(client.user.username, client.user.avatarURL)
-      .setDescription(`Kanal koruma zaten ayarlanmamış!`);
+      .setDescription(`Lütfen bir sınır belirtiniz!`);
     message.channel.send(embed);
     return;
   }
-  db.delete(`kanalk_${message.guild.id}`);
+  db.set(`slimido_${message.guild.id}`, kanal);
   const embed = new Discord.RichEmbed()
     .setColor("BLACK")
     .setFooter(client.user.username, client.user.avatarURL)
-    .setDescription(`Kanal koruma sistemi sıfırlandı!`);
+    .setDescription(
+      `Ban limit; ${kanal} olarak ayarlandı!\nNot: Eğer ayarlardan herhangi bir rol ayarlanmadıysa limit işlemez!`
+    );
   message.channel.send(embed);
   return;
 };
@@ -25,12 +27,12 @@ module.exports.run = async (client, message, args) => {
 exports.conf = {
   enabled: true,
   guildOnly: true,
-  aliases: ["channel-protection-reset"],
+  aliases: [],
   permLevel: 3
 };
 
 exports.help = {
-  name: "kanal-koruma-sıfırla",
-  description: "kanal-koruma-sıfırla",
-  usage: "kanal-koruma-sıfırla"
+  name: "ban-limit",
+  description: "ban-limit",
+  usage: "ban-limit"
 };
