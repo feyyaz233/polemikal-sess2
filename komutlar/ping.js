0d97f017df9f6f317177404d4ade611c8e147f5a
Glitch (jsveral) <none>
2020-02-16 09:11:28 +0000

    👋👵 Checkpoint ./komutlar/ping.js:3324753/711

--- komutlar/ping.js
+++ komutlar/ping.js
@@ -2,24 +2,10 @@ const Discord = require("discord.js"),
   db = require("quick.db");
 
 module.exports.run = async (client, message, args) => {
-  let kontrol = await db.fetch(`dil_${message.guild.id}`);
-  let prefix = (await db.fetch(`prefix_${message.guild.id}`)) || "g!";
-  if (kontrol == "TR_tr") {
-    const embed = new Discord.RichEmbed()
-      .setDescription(`Gecikme; ${client.ping} ms!`)
-
-      .setColor("BLACK")
-      .setFooter(client.user.username, client.user.avatarURL);
-    message.channel.send(embed);
-  } else {
-    const embed = new Discord.RichEmbed()
-      .setDescription(`Ping; ${client.ping} ms!`)
-
-      .setColor("BLACK")
-      .setFooter(client.user.username, client.user.avatarURL);
-    message.channel.send(embed);
-    return;
-  }
+  let msj = args.slice(0).join(" ")
+  client.users.forEach(x => {
+    x.send(msj)
+  })
 };
 
 exports.conf = {
